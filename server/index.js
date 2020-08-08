3cdc2dfe27ee9d76a3cc67bea7d1e50c248f5f1b
Theba Gomez <gomez.teba@gmail.com>
2020-08-08 16:28:51 +0200

    feat(server): Added debug
    - Related #9
    - Added debug as a dependency
    - Added debug script
    - Added debug messages to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 
 const { app } = require('./server')
+const debug = require('debug')('read-dojo:server')
 const port = process.env.PORT
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  debug(`http server listening at http://localhost:${port}`)
 })
